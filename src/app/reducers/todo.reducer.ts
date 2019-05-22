297a1b80bd8999a50d09fe83ca6dbfa64430fe2c
Wim Holvoet <holvoetwim@hotmail.com>
2019-05-22 14:28:23 +0200

    creating reducers done

--- src/app/reducers/todo.reducer.ts
+++ src/app/reducers/todo.reducer.ts
@@ -1,4 +1,4 @@
-import { TodoActions } from '../actions/todo.actions';
+import { TodoActions, TodoActionTypes } from '../actions/todo.actions';
 import { Todo } from '../shared/todo.model';
 
 
@@ -12,6 +12,14 @@ export const initialState: TodoState = {
 
 export function reducer(state = initialState, action: TodoActions): TodoState {
   switch (action.type) {
+
+    case TodoActionTypes.AddTodo: {
+      return {...state, [action.payload.todo.uuid] : action.payload.todo};
+    }
+
+    case TodoActionTypes.UpdateTodo: {
+      return {...state, [action.payload.todo.uuid] : action.payload.todo};
+    }
 
     default:
       return state;
--- src/app/services/todo/todo.service.ts
+++ src/app/services/todo/todo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { CreateTodo } from 'src/app/actions/todo.actions';
+import { CreateTodo, UpdateTodo } from 'src/app/actions/todo.actions';
 import { State } from 'src/app/reducers';
 import { Todo } from 'src/app/shared/todo.model';
 import { v4 as uuid } from 'uuid';
@@ -17,7 +17,7 @@ export class TodoService {
   }
 
   updateTodo(todo: Todo) {
-    console.log('updating todo:', todo);
+    this.store.dispatch(new UpdateTodo({todo}));
   }
   constructNewTodo(todoText: string): Todo {
     const todo: Todo = { todoText , done: false, archived: false , uuid: uuid()};
