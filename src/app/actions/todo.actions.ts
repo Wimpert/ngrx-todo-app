af275e2ba9634060dc2cd70a70409a595b7bba30
Wim Holvoet <holvoetwim@hotmail.com>
2019-05-22 12:42:20 +0200

    added todos

--- src/app/actions/todo.actions.ts
+++ src/app/actions/todo.actions.ts
@@ -1,14 +1,26 @@
 import { Action } from '@ngrx/store';
+import { Todo } from '../shared/todo.model';
 
 export enum TodoActionTypes {
-  LoadTodos = '[Todo] Load Todos',
-  
-  
+  CreateTodo = '[Todo] Create Todo',
+  AddTodo = '[Todo] Add Todo',
+  UpdateTodo = '[Todo] Update Todo',
+
 }
 
-export class LoadTodos implements Action {
-  readonly type = TodoActionTypes.LoadTodos;
+export class CreateTodo implements Action {
+  readonly type = TodoActionTypes.CreateTodo;
+  payload: {todoText: string};
+}
+export class AddTodo implements Action {
+  readonly type = TodoActionTypes.AddTodo;
+  payload: { todo: Todo};
+}
+
+export class UpdateTodo implements Action {
+  readonly type = TodoActionTypes.UpdateTodo;
+  payload: { todo: Todo};
 }
 
 
-export type TodoActions = LoadTodos;
+export type TodoActions = AddTodo | CreateTodo | UpdateTodo;
